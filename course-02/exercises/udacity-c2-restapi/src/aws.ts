bae4782fa9000c5d746a9a9c51ab619e783a4f17
lnguyen-cpi <nblam1994@gmail.com>
2019-10-10 22:12:18 -0700

    Finish proj image-filter with working url

--- course-02/exercises/udacity-c2-restapi/src/aws.ts
+++ course-02/exercises/udacity-c2-restapi/src/aws.ts
@@ -1,11 +1,18 @@
 import AWS = require('aws-sdk');
 import { config } from './config/config';
 
-const c = config.dev;
+const c = config.aws;
 
 //Configure AWS
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
+// var credentials = new AWS.SharedIniFileCredentials({profile: c.aws_profile});
+// AWS.config.credentials = credentials;
+
+AWS.config = new AWS.Config({
+  accessKeyId: '', 
+  secretAccessKey: '', 
+  region: 'us-west-1'
+});
+
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
