9ff83b378b8a38c224f432f4495f58b9dcdb1c08
Lam Nguyen <nblam1994@gmail.com>
2019-10-14 20:56:26 -0700

    Revert "style: use typescript when possible"

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import {Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 import { fstat } from 'fs';
@@ -29,10 +28,9 @@ import { fstat } from 'fs';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  app.get("/filteredimage", async (req: Request, res: Response) => {
+  app.get("/filteredimage", async (req, res) => {
 
-      let image_url:string = req.query.image_url;
-
+      let {image_url} = req.query;
       if (!image_url)
         return res.status(400).send(`image_url is required`);
 
@@ -50,7 +48,7 @@ import { fstat } from 'fs';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req: Request, res: Response) => {
+  app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
